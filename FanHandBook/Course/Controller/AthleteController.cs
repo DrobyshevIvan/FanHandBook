690d029350fff2cb8195bcafe07d2e68ddb4d63d
DrobyshevIvan <vanyadrob0@gmail.com>
2024-06-11 22:17:31 +0300

    update

---
+++ FanHandBook/Course/Controller/AthleteController.cs
@@ -0,0 +1,177 @@
+﻿using Course.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course.Controller
+{
+    public class AthleteController
+    {
+        private readonly string _filePath;
+        private List<Athlete> _athletes;
+
+        public AthleteController(string filePath)
+        {
+            _filePath = filePath;
+            _athletes = DataManagement.LoadAthletesFromFile(_filePath);
+        }
+
+        public List<Athlete> GetAthletes()
+        {
+            return _athletes;
+        }
+
+        public void SaveData()
+        {
+            DataManagement.SaveAthletesToFile(_filePath, _athletes);
+        }
+
+        public void AddAthlete(Athlete athlete) 
+        {
+            _athletes.Add(athlete);
+            SaveData();
+        }
+
+        public void UpdateAthlete(Athlete athlete)
+        {
+            var index = _athletes.FindIndex(a => a.AthleteId == athlete.AthleteId);
+            if (index != -1)
+            {
+                _athletes[index] = athlete;
+                SaveData();
+            }
+        }
+
+        public void RemoveAthlete(string athleteId)
+        {
+            var athleteToRemove = _athletes.FirstOrDefault(a => a.AthleteId == athleteId);
+            if (athleteToRemove != null)
+            {
+                _athletes.Remove(athleteToRemove);
+                SaveData();
+            }
+            else
+            {
+                throw new Exception("Athlete not found.");
+            }
+        }
+
+        private bool ContainsFullName(Athlete athlete, string name)
+        {
+            string fullName = athlete.FirstName + " " + athlete.LastName;
+            return fullName.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        public List<Athlete> FilterAthletesByName(string name)
+        {
+            return _athletes.Where(a => ContainsFullName(a, name)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByCountry(string country)
+        {
+            return _athletes.Where(a => a.Country.IndexOf(country, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+        }
+
+        public List<Athlete> FilterAthletesBySport(string sport)
+        {
+            return _athletes.Where(a => a.Sport.IndexOf(sport, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByHeight(double minHeight, double maxHeight)
+        {
+            return _athletes.Where(a => a.Height >= minHeight && a.Height <= maxHeight).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByRecordHolders(string achievement)
+        {
+            if (string.IsNullOrWhiteSpace(achievement))
+            {
+                return _athletes.Where(a => a.PersonalRecords.Any(pr => pr.IndexOf("wr", StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+            }
+            else
+            {
+                return _athletes.Where(a => a.PersonalRecords.Any(pr => pr.IndexOf(achievement, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+            }
+        }
+
+        public List<Athlete> FilterAthletesByAchievements(string achievement)
+        {
+            return _athletes.Where(a => a.PersonalRecords.Any(pr => pr.IndexOf(achievement.Trim(), StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByNameAndHeight(string name, double minHeight, double maxHeight)
+        {
+            return _athletes.Where(a => ContainsFullName(a, name) && a.Height >= minHeight && a.Height <= maxHeight).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByNameAndRecordHolders(string name, string records)
+        {
+            return _athletes.Where(a => ContainsFullName(a, name) && a.PersonalRecords.Any(pr => pr.IndexOf("wr", StringComparison.OrdinalIgnoreCase) >= 0 && pr.IndexOf(records, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByNameAndAchievements(string name, string achievement)
+        {
+            return _athletes.Where(a => ContainsFullName(a, name) && a.PersonalRecords.Any(pr => pr.IndexOf(achievement, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByCountryAndHeight(string country, double minHeight, double maxHeight)
+        {
+            return _athletes.Where(a => a.Country.IndexOf(country, StringComparison.OrdinalIgnoreCase) >= 0 && a.Height >= minHeight && a.Height <= maxHeight).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByCountryAndRecordHolders(string country, string records)
+        {
+            return _athletes.Where(a => a.Country.IndexOf(country, StringComparison.OrdinalIgnoreCase) >= 0 && a.PersonalRecords.Any(pr => pr.IndexOf("wr", StringComparison.OrdinalIgnoreCase) >= 0 && pr.IndexOf(records, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesByCountryAndAchievements(string country, string achievement)
+        {
+            return _athletes.Where(a => a.Country.IndexOf(country, StringComparison.OrdinalIgnoreCase) >= 0 && a.PersonalRecords.Any(pr => pr.IndexOf(achievement, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesBySportAndHeight(string sport, double minHeight, double maxHeight)
+        {
+            return _athletes.Where(a => a.Sport.IndexOf(sport, StringComparison.OrdinalIgnoreCase) >= 0 && a.Height >= minHeight && a.Height <= maxHeight).ToList();
+        }
+
+        public List<Athlete> FilterAthletesBySportAndRecordHolders(string sport, string records)
+        {
+            return _athletes.Where(a => a.Sport.IndexOf(sport, StringComparison.OrdinalIgnoreCase) >= 0 && a.PersonalRecords.Any(pr => pr.IndexOf("wr", StringComparison.OrdinalIgnoreCase) >= 0 && pr.IndexOf(records, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public List<Athlete> FilterAthletesBySportAndAchievements(string sport, string achievement)
+        {
+            return _athletes.Where(a => a.Sport.IndexOf(sport, StringComparison.OrdinalIgnoreCase) >= 0 && a.PersonalRecords.Any(pr => pr.IndexOf(achievement, StringComparison.OrdinalIgnoreCase) >= 0)).ToList();
+        }
+
+        public void SaveAthleteToTextFile(Athlete athlete, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine($"Ім'я: {athlete.FirstName} {athlete.LastName}");
+                writer.WriteLine($"Дата народження: {athlete.DateOfBirth.ToShortDateString()}");
+                writer.WriteLine($"Зріст: {athlete.Height} cm");
+                writer.WriteLine($"Вага: {athlete.Weight} kg");
+                writer.WriteLine($"Країна: {athlete.Country}");
+                writer.WriteLine($"Спорт: {athlete.Sport}");
+                writer.WriteLine($"Клуб або команда: {athlete.ClubOrTeam}");
+                writer.WriteLine($"Тренер: {athlete.Coach}");
+                writer.WriteLine();
+
+                writer.WriteLine("Досягнення:");
+                foreach (var record in athlete.PersonalRecords)
+                {
+                    writer.WriteLine($"- {record}");
+                }
+                writer.WriteLine();
+
+                writer.WriteLine("Ігри/Змагання:");
+                foreach (var game in athlete.Games)
+                {
+                    writer.WriteLine($"{game.Date.ToShortDateString()} - {game.Opponent} - {game.Result}");
+                }
+            }
+        }
+    }
+}
+
---
+++ FanHandBook/Course/View/AthleteDetailsForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿using System.ComponentModel;
+
+namespace Course.View
+{
+    partial class AthleteDetailsForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            pictureBoxPhoto = new PictureBox();
+            lblDateOfBirth = new Label();
+            lblName = new Label();
+            lblHeight = new Label();
+            lblWeight = new Label();
+            lblCountry = new Label();
+            lblSport = new Label();
+            lblClubOrTeam = new Label();
+            lblCoach = new Label();
+            panelPersonalRecords = new Panel();
+            lblPersonalRecords = new Label();
+            dataGridViewGames = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
+            btnDeleteGame = new Button();
+            btnAddGame = new Button();
+            btnEdit = new Button();
+            btnSaveToTextFile = new Button();
+            ((ISupportInitialize)pictureBoxPhoto).BeginInit();
+            panelPersonalRecords.SuspendLayout();
+            ((ISupportInitialize)dataGridViewGames).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxPhoto
+            // 
+            pictureBoxPhoto.BorderStyle = BorderStyle.Fixed3D;
+            pictureBoxPhoto.Location = new Point(10, 10);
+            pictureBoxPhoto.Name = "pictureBoxPhoto";
+            pictureBoxPhoto.Size = new Size(100, 100);
+            pictureBoxPhoto.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxPhoto.TabIndex = 0;
+            pictureBoxPhoto.TabStop = false;
+            // 
+            // lblDateOfBirth
+            // 
+            lblDateOfBirth.Location = new Point(120, 120);
+            lblDateOfBirth.Name = "lblDateOfBirth";
+            lblDateOfBirth.Size = new Size(400, 20);
+            lblDateOfBirth.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            lblName.ForeColor = Color.Black;
+            lblName.Location = new Point(120, 97);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(400, 23);
+            lblName.TabIndex = 2;
+            // 
+            // lblHeight
+            // 
+            lblHeight.Location = new Point(120, 140);
+            lblHeight.Name = "lblHeight";
+            lblHeight.Size = new Size(400, 20);
+            lblHeight.TabIndex = 3;
+            // 
+            // lblWeight
+            // 
+            lblWeight.Location = new Point(120, 160);
+            lblWeight.Name = "lblWeight";
+            lblWeight.Size = new Size(400, 20);
+            lblWeight.TabIndex = 4;
+            // 
+            // lblCountry
+            // 
+            lblCountry.Location = new Point(120, 180);
+            lblCountry.Name = "lblCountry";
+            lblCountry.Size = new Size(400, 20);
+            lblCountry.TabIndex = 5;
+            // 
+            // lblSport
+            // 
+            lblSport.Location = new Point(120, 200);
+            lblSport.Name = "lblSport";
+            lblSport.Size = new Size(400, 20);
+            lblSport.TabIndex = 6;
+            // 
+            // lblClubOrTeam
+            // 
+            lblClubOrTeam.Location = new Point(120, 220);
+            lblClubOrTeam.Name = "lblClubOrTeam";
+            lblClubOrTeam.Size = new Size(400, 20);
+            lblClubOrTeam.TabIndex = 7;
+            // 
+            // lblCoach
+            // 
+            lblCoach.Location = new Point(120, 240);
+            lblCoach.Name = "lblCoach";
+            lblCoach.Size = new Size(400, 20);
+            lblCoach.TabIndex = 8;
+            // 
+            // panelPersonalRecords
+            // 
+            panelPersonalRecords.AutoScroll = true;
+            panelPersonalRecords.BorderStyle = BorderStyle.FixedSingle;
+            panelPersonalRecords.Controls.Add(lblPersonalRecords);
+            panelPersonalRecords.Location = new Point(532, 10);
+            panelPersonalRecords.Name = "panelPersonalRecords";
+            panelPersonalRecords.Size = new Size(250, 250);
+            panelPersonalRecords.TabIndex = 9;
+            // 
+            // lblPersonalRecords
+            // 
+            lblPersonalRecords.Location = new Point(-1, 0);
+            lblPersonalRecords.Name = "lblPersonalRecords";
+            lblPersonalRecords.Size = new Size(250, 250);
+            lblPersonalRecords.TabIndex = 10;
+            // 
+            // dataGridViewGames
+            // 
+            dataGridViewGames.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewGames.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewGames.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3 });
+            dataGridViewGames.Location = new Point(10, 300);
+            dataGridViewGames.Name = "dataGridViewGames";
+            dataGridViewGames.RowHeadersWidth = 51;
+            dataGridViewGames.Size = new Size(760, 150);
+            dataGridViewGames.TabIndex = 10;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewCellStyle1.Format = "dd.MM.yyyy";
+            dataGridViewTextBoxColumn1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewTextBoxColumn1.HeaderText = "Дата";
+            dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "Суперник/Змагання";
+            dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            dataGridViewTextBoxColumn3.HeaderText = "Результат";
+            dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // btnDeleteGame
+            // 
+            btnDeleteGame.Location = new Point(12, 456);
+            btnDeleteGame.Name = "btnDeleteGame";
+            btnDeleteGame.Size = new Size(85, 32);
+            btnDeleteGame.TabIndex = 11;
+            btnDeleteGame.Text = "Видалити";
+            btnDeleteGame.UseVisualStyleBackColor = true;
+            btnDeleteGame.Click += btnDeleteGame_Click;
+            // 
+            // btnAddGame
+            // 
+            btnAddGame.Location = new Point(103, 457);
+            btnAddGame.Name = "btnAddGame";
+            btnAddGame.Size = new Size(72, 31);
+            btnAddGame.TabIndex = 12;
+            btnAddGame.Text = "Додати";
+            btnAddGame.UseVisualStyleBackColor = true;
+            btnAddGame.Click += btnAddGame_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(682, 457);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(106, 36);
+            btnEdit.TabIndex = 13;
+            btnEdit.Text = "Редагувати";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnSaveToTextFile
+            // 
+            btnSaveToTextFile.Location = new Point(532, 457);
+            btnSaveToTextFile.Name = "btnSaveToTextFile";
+            btnSaveToTextFile.Size = new Size(145, 36);
+            btnSaveToTextFile.TabIndex = 14;
+            btnSaveToTextFile.Text = "Зберегти у файл";
+            btnSaveToTextFile.UseVisualStyleBackColor = true;
+            btnSaveToTextFile.Click += btnSaveToTextFile_Click;
+            // 
+            // AthleteDetailsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 500);
+            Controls.Add(btnSaveToTextFile);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAddGame);
+            Controls.Add(pictureBoxPhoto);
+            Controls.Add(lblDateOfBirth);
+            Controls.Add(lblName);
+            Controls.Add(lblHeight);
+            Controls.Add(lblWeight);
+            Controls.Add(lblCountry);
+            Controls.Add(lblSport);
+            Controls.Add(lblClubOrTeam);
+            Controls.Add(lblCoach);
+            Controls.Add(panelPersonalRecords);
+            Controls.Add(dataGridViewGames);
+            Controls.Add(btnDeleteGame);
+            Name = "AthleteDetailsForm";
+            Text = "Детальна інформація про спортсмена";
+            ((ISupportInitialize)pictureBoxPhoto).EndInit();
+            panelPersonalRecords.ResumeLayout(false);
+            ((ISupportInitialize)dataGridViewGames).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblName, lblDateOfBirth, lblHeight, lblWeight, lblCountry, lblSport, lblClubOrTeam, lblCoach, lblPersonalRecords;
+        private PictureBox pictureBoxPhoto;
+        private DataGridView dataGridViewGames;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private Panel panelPersonalRecords;
+        private Button btnDeleteGame, btnAddGame, btnEdit, btnSaveToTextFile;
+    }
+}
